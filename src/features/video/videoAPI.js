8270a59d70c1ee06a7d370094f5c756de426b3e0
Zij Zahidul Islam Jiku <safzahidul.cse@gmail.com>
2023-04-14 02:27:58 +0600

    Video Description build

--- src/features/video/videoAPI.js
+++ src/features/video/videoAPI.js
@@ -1,7 +1,7 @@
 import axios from "../../utils/axios";
 
 export const getVideo = async (id) => {
-  const response = await axios.get(`/video/${id}`);
+  const response = await axios.get(`/videos/${id}`);
 
   return response.data;
 }
