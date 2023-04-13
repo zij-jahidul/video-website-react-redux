96e9263871a9dd0e6b947d34fc18f9ec8a7b98a7
Zij Zahidul Islam Jiku <safzahidul.cse@gmail.com>
2023-04-14 01:16:24 +0600

    Tags and Videos Slice and store build

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import tagsReducer from '../features/tags/tagsSlice';
+import videosReducer from '../features/videos/videosSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    tags: tagsReducer,
+    videos: videosReducer,
   },
 });
